4cd64453bbddaeda450443a5f3cb61767d80ebb1
Ruben S <ruben.speybrouck@siemens-energy.com>
2024-02-10 19:51:37 +0100

    Fixed bugs in total return calc

--- program/streamlit_functions/etf_performance/compare_asset_performance.py
+++ program/streamlit_functions/etf_performance/compare_asset_performance.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from program.workers.calculate_total_return import calculate_annualized_return_with_dividends
+import pandas as pd
 
 
 def analyse_asset_performance():
     st.header('Analyse asset performance')
- 
+
     tickers = st.text_input('Enter tickers (comma separated)')
-    
 
     start_year = st.slider('Start year', 1980, 2022, 2005)
 
@@ -17,22 +17,20 @@ def analyse_asset_performance():
         tickers = [ticker.upper() for ticker in tickers]
         st.write(f'Selected tickers: {tickers}')
 
-        # CREATE A STREAMLIT COLumn for each ticker
+        # CREATE A STREAMLIT CoLumn for each ticker
 
         columns = st.columns(len(tickers))
 
         for idx, ticker in enumerate(tickers):
-        
-
 
             with st.spinner(f'Loading {ticker}...'):
-                annualized_return = calculate_annualized_return_with_dividends(ticker, start_year)
+                annualized_return, warning = calculate_annualized_return_with_dividends(
+                    ticker, start_year)
                 columns[idx].write(f'Annualized return for {ticker}:')
+                if warning is not None:
+                    columns[idx].warning(warning)
+                else:
+                    columns[idx].success(
+                        f'Successfully calculated annualized return from {start_year}  to {pd.Timestamp.today().year - 1}')
                 columns[idx].write(annualized_return)
                 columns[idx].write('---')
-
-
-
-
-
-
--- program/workers/calculate_total_return.py
+++ program/workers/calculate_total_return.py
@@ -1,45 +1,63 @@
 import yfinance as yf
 import pandas as pd
 import json
+import streamlit as st
 
 
-def calculate_annualized_return_with_dividends(stock_ticker, start_year=None) -> dict:
+def calculate_annualized_return_with_dividends(stock_ticker, start_year: int | None = None) -> tuple[dict, str | None]:
+
+    # A place to store warning or status messages.
+    warning = None
+
+    print(
+        f'calculating annualised return for stock_ticker: {stock_ticker} and start_year: {start_year}')
 
     # Fetch historical data for the stock from the earliest available date
     stock = yf.Ticker(stock_ticker)
-    stock_data = stock.history(period="max")  # 
+    stock_data = stock.history(period="max")  #
     # print(f'stock_data: {stock_data}')
     # Get inception date (first date in the data)
     inception_date = stock_data.index[0]
 
     # Get the year of the inception date
-    inception_year = inception_date.year
+    inception_year: int = int(inception_date.year)
+
+    # print(f'inception_year: {inception_year} of stock {stock_ticker}')
 
     # Find the number of years between 01/01/inception year +1 and today
     today = pd.Timestamp.today()
 
     # if we have a start year, use that instead of the inception year
-    if start_year is None:  
+    if start_year is None:
         years = today.year - inception_year
+        start_year = inception_year
     else:
+        print(f'start_year: {start_year}')
+        print(f'inception_year: {inception_year}')
+        print(start_year <= inception_year)
+        if int(start_year) <= inception_year:
+            start_year = inception_year
+            warning = f'Start year was before inception year. Using inception year {inception_year} instead.'
         years = today.year - start_year
-        
     max_drawdown = 0
     max_drawdown_date = None
     highest_price = None
     data_per_year = {}
     # Loop through each year and calculate the annualized return
-    for year in range(1, years):
-        
-        selected_year = inception_year + year
+    for year in range(0, years):
+
+        selected_year = start_year + year
+        # print(f'selected_year: {selected_year} of stock {stock_ticker}')
         # Filter the dataframe to include rows where the date columns contains the given year
-        stock_data_for_year = stock_data[stock_data.index.year == selected_year] # type: ignore
+        # type: ignore
+        stock_data_for_year = stock_data[stock_data.index.year ==
+                                         selected_year]
         # Initialize the number of shares and total investment value
         initial_investment = stock_data_for_year['Close'][0]
         if highest_price is None:
             highest_price = initial_investment
         shares_owned = 1  # Start with one share for simplicity
-        
+
         dividends = {}
 
         for date, row in stock_data_for_year.iterrows():
@@ -59,7 +77,8 @@ def calculate_annualized_return_with_dividends(stock_ticker, start_year=None) ->
                 shares_owned += reinvested_shares
 
         # Calculate final investment value
-        final_investment_value = shares_owned * stock_data_for_year['Close'][-1]
+        final_investment_value = shares_owned * \
+            stock_data_for_year['Close'][-1]
         # print(f'final_investment_value: {final_investment_value}')
 
         # Calculate total return
@@ -69,8 +88,8 @@ def calculate_annualized_return_with_dividends(stock_ticker, start_year=None) ->
         # print(f"Total return for {selected_year}: {total_return:.2%}")
         data_per_year[selected_year]['total_return'] = total_return
         # data_per_year[selected_year]['dividends'] = dividends
-        data_per_year[selected_year]['dividend_yield'] = sum(dividends.values()) / initial_investment
-
+        data_per_year[selected_year]['dividend_yield'] = sum(
+            dividends.values()) / initial_investment
 
        # Calculate average return
     average_return = sum([data['total_return']
@@ -80,8 +99,7 @@ def calculate_annualized_return_with_dividends(stock_ticker, start_year=None) ->
     def calculate_past_return(n):
         return sum([data['total_return'] for data in list(data_per_year.values())[-n:]]) / min(n, len(data_per_year))
 
-  
-    return_past_3_years =  calculate_past_return(3) if years > 3 else None
+    return_past_3_years = calculate_past_return(3) if years > 3 else None
     return_past_5_years = calculate_past_return(5) if years > 5 else None
     return_past_10_years = calculate_past_return(10) if years > 10 else None
     return_past_7_years = calculate_past_return(7) if years > 7 else None
@@ -103,7 +121,5 @@ def calculate_annualized_return_with_dividends(stock_ticker, start_year=None) ->
         'return_past_25_years': return_past_25_years,
         'return_per_year': data_per_year,
     }
-   
-    return performance
 
-
+    return performance, warning
